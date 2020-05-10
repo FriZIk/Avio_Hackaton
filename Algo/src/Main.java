1a6c3f29ed7fc260cc3279e626f3d9ee7167e9ef
Drelagreen <44087347+drelagreen@users.noreply.github.com>
2020-05-10 13:46:34 +0300

    Алгоритм распаковки KMZ, передачи фото, передачи координат

---
+++ Algo/src/Main.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class Main {
+    static String IP = "0.tcp.ngrok.io";
+    static int PORT = 17748;
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket(IP, PORT);
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            ZipFile zipFile = new ZipFile(new File("./v1.kmz"), ZipFile.OPEN_READ);
+            ZipEntry entry = zipFile.getEntry("doc.kml");
+
+            String str = new String(zipFile.getInputStream(entry).readAllBytes());
+            String splited = str.split("LatLonAltBox")[1];
+
+            byte[] splB = splited.getBytes();
+            BigInteger bigInteger1 = BigInteger.valueOf(splB.length);
+
+            File testImg = new File("./test.jpg");
+            FileInputStream fileInputStream = new FileInputStream(testImg);
+            byte[] imgB = fileInputStream.readAllBytes();
+            BigInteger bigInteger2 = BigInteger.valueOf(imgB.length);
+
+            byte[] bar1 = new byte[4];
+            byte[] bar11 = bigInteger1.toByteArray();
+
+            byte[] bar2 = new byte[4];
+            byte[] bar22 = bigInteger2.toByteArray();
+
+            for (int i = 3, k = bar11.length-1, j = bar22.length-1; i>0; i--){
+                if (k>=0){
+                    bar1[i] = bar11[k--];
+                }  else {
+                    bar1[i] = 0;
+                }
+                if (j>=0){
+                    bar2[i] = bar22[j--];
+                }  else {
+                    bar2[i] = 0;
+                }
+            }
+
+            outputStream.write(bar1);
+            outputStream.write(bar2);
+
+            outputStream.write(splB);
+            outputStream.write(imgB);
+
+            byte[] fourBytes = new byte[4];
+            inputStream.read(fourBytes);
+            int length = ByteBuffer.wrap(fourBytes).getInt();
+            System.out.println(length);
+
+            byte[] strBytes = new byte[length];
+            inputStream.read(strBytes);
+            String s = new String(strBytes);
+            System.out.println(s);
+
+//            dataInputStream.readUTF();
+            byte[] b = inputStream.readAllBytes();
+
+            File file = new File("IMAGE.png");
+            if (!file.exists()) file.createNewFile();
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(b);
+            fileOutputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
